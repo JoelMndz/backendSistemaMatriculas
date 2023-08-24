bdca10524800da13e1e3ef6f082e0e327fda7240
JoelMndz <luisjo3lml@gmail.com>
2023-08-24 11:24:53 -0500

    conflicto resuelto

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,11 +10,8 @@ import { SchoolTermModule } from './school-term/school-term.module';
 import { StorageModule } from './storage/storage.module';
 import { RepresentativeModule } from './representative/representative.module';
 import { ProfessorModule } from './professor/professor.module';
-<<<<<<< HEAD
 import { StudentModule } from './student/student.module';
-=======
 import { ParallelModule } from './parallel/parallel.module';
->>>>>>> c2b7f7abc2ba3488cc36b8a62d7ddb78dcee4def
 
 @Module({
   imports: [
@@ -28,11 +25,8 @@ import { ParallelModule } from './parallel/parallel.module';
     StorageModule,
     RepresentativeModule,
     ProfessorModule,
-<<<<<<< HEAD
     StudentModule,
-=======
     ParallelModule,
->>>>>>> c2b7f7abc2ba3488cc36b8a62d7ddb78dcee4def
   ],
   controllers: [],
   providers: [],
